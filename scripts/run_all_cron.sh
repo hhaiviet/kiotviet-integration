bc8589b91c052cd5000bd7115d0d6f613cd0afd6
hhaiviet <hhaiviet@gmail.com>
2025-10-08 14:25:11 +0000

    Remove cron script with secrets

--- scripts/run_all_cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export AZURE_STORAGE_CONNECTION_STRING='DefaultEndpointsProtocol=https;AccountName=kiotvietintegration;AccountKey=FEigNAELqRRYMuJRvTKqJM//Y3L7KUd8mmz0rEA4/0yx7fUQ5kEwMAxFUe2XoV/GsbinmsgHdgv7+ASt/oir6A==;EndpointSuffix=core.windows.net'
-export AZURE_STORAGE_CONTAINER="kiotviet-data"
-export KIOTVIET_USERNAME="0913431718"
-export KIOTVIET_PASSWORD="68686868"
-
-/home/codespace/.python/current/bin/python3 /workspaces/kiotviet-integration/scripts/kiotviet_run_all.py >> /workspaces/kiotviet-integration/cron.log 2>&1
